b3c2dfbbc6fd9f1255d63762c90ae8367117db9d
zee-german <franz.lutzenberger@gmail.com>
2016-04-19 11:48:04 +0100

    custom inputs & validation

---
+++ client/accounts/Accounts.js
@@ -0,0 +1,29 @@
+AccountsTemplates.addFields([
+  {
+    _id: 'firstName',
+    type: 'text',
+    displayName: 'First Name',
+    required: true,
+    re: /(?=.*[a-z])(?=.*[A-Z])/,
+    errStr: 'At least 1 lowercase and 1 uppercase letter required'
+  },
+  {
+    _id: 'profession',
+    type: 'select',
+    displayName: 'Profession',
+    select: [
+      {
+        text: 'Developer',
+        value: 'developer'
+      },
+      {
+        text: 'Designer',
+        value: 'designer'
+      },
+      {
+        text: 'Other',
+        value: 'other'
+      }
+    ]
+  }
+])
